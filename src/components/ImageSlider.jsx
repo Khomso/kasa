aadc4d3d5b384c1ca3dc46cf3af9a3023af69ba5
kong <k_sokhom@hotmail.fr>
2022-11-16 19:03:49 +0100

    slider fonctionnel

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,80 +1,47 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import "../styles/ImageSlider.css"
 
-const ImageSlider = ({ slides }) => {
-  // const img = useState(0);
-  const [currentIndex, setCurrentIndex] = useState(0);
+const ImageSlider = (props) => {
+  const { slides } = props;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const sliderStyles = {
-    height: "100%",
-    position: "relative",
-  };
-  const slideStyles = {
-    width: "100%",
-    height: "100%",
-    borderRadius: "10px",
-    backgroundSize: "cover",
-    backgroundImage: `url(${slides[currentIndex].url})`,
-  };
+  const slidePrev = () => {
+    setCurrentImageIndex((index) => {
+      const nextIndex = index - 1;
 
-  const leftArrow = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0, -50%)",
-    left: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: 1,
-    cursor: "pointer",
+      if (nextIndex < 0) {
+        return slides.length - 1;
+      }
+      return nextIndex;
+    });
   };
-  const rightArrow = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0, -50%)",
-    right: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: 1,
-    cursor: "pointer",
+  const slideNext = () => {
+    setCurrentImageIndex((index) => {
+      const nextIndex = index + 1;
+      if (nextIndex >= slides.length) {
+        return 0;
+      }
+      return nextIndex;
+    });
   };
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-  const dotsContainerStyles = {
-    display: "flex",
-    justifyContent: "center",
-  };
-  const dotStyles = {
-    margin: "0 3px",
-    cursor: "pointer",
-    fontSize: "20px",
-  };
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  }
 
   return (
-    <div style={sliderStyles}>
-      <div style={leftArrow} onClick={goToPrevious}>
-        v
-      </div>
-      <div style={rightArrow} onClick={goToNext}>
-        v
-      </div>
-      <div style={slideStyles}></div>
-      <div styles={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => (
-          <div key={slideIndex} style={dotStyles} onClick= {goToSlide(slideIndex)} >.....</div>
-        ))}
-      </div>
+    <div
+      className="slide"
+      style={{
+        background: `url(${slides[currentImageIndex]}) center/cover no-repeat`,
+
+      }}
+    >
+      <button className="prev" onClick={slidePrev}>
+        &lt;
+      </button>
+      <button className="next" onClick={slideNext}>
+        &gt;
+      </button>
+      <p className="count">
+        {currentImageIndex + 1} / {slides.length}
+      </p>
     </div>
   );
 };
